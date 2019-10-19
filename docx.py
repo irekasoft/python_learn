9c1e558fc894f583834b69190fd88953c081e7f3
Hijazi (MBP, Retina) <irekasoft@gmail.com>
2019-10-19 22:50:41 +0800

    add csv

--- docx.py
+++ docx.py
@@ -33,6 +33,7 @@ hdr_cells = table.rows[0].cells
 hdr_cells[0].text = 'Qty'
 hdr_cells[1].text = 'Id'
 hdr_cells[2].text = 'Desc'
+
 for qty, id, desc in records:
     row_cells = table.add_row().cells
     row_cells[0].text = str(qty)
